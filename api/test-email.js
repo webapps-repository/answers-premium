a963b9799820380ef3e939e476f67d4f7bae288b
webapps-repository <henry@hazcam.io>
2025-11-09 11:20:17 +1000

    Update test-email.js

--- api/test-email.js
+++ api/test-email.js
@@ -1,9 +1,5 @@
 // api/test-email.js
 
-// api/test-email.js
-
-// api/test-email.js
-
 import { sendEmailWithAttachment } from './utils/sendEmail.js';
 
 export default async function handler(req, res) {
