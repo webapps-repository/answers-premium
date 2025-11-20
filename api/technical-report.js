090664f9a4a52c8d36ab3f30d1e3980a322f44e3
webapps-repository <henry@hazcam.io>
2025-11-20 12:08:09 +1000

    Update technical-report.js

--- api/technical-report.js
+++ api/technical-report.js
@@ -6,10 +6,7 @@ import fs from "fs";
 
 import { generateInsights } from "../lib/insights.js";
 import { generatePDF } from "../lib/pdf.js";
-import {
-  sendEmailHTML,
-  validateUploadedFile
-} from "../lib/utils.js";
+import { verifyRecaptcha, sendEmailHTML, validateUploadedFile } from "../lib/utils.js";
 
 export const config = { api: { bodyParser: false } };
 
