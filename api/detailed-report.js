e784e7e75c50332d38888c82960863eecbec4de8
webapps-repository <henry@hazcam.io>
2025-11-20 16:18:58 +1000

    Update detailed-report.js

--- api/detailed-report.js
+++ api/detailed-report.js
@@ -11,6 +11,12 @@ export const config = {
   }
 };
 
+if (req.method === "OPTIONS") {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  return res.status(200).end();
+}
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
